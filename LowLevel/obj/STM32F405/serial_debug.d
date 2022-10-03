d46477051ebdd025934e2c33684ed6c44de1112f
simondlevy <simon.d.levy@gmail.com>
2022-10-03 18:57:55 -0400

    =Cleanup

--- LowLevel/obj/STM32F405/serial_debug.d
+++
@@ -1,4 +0,0 @@
-obj/STM32F405/serial_debug.o: stm/src/serial_debug.c hfsrc/serial.h \
- hfsrc/time.h
-hfsrc/serial.h:
-hfsrc/time.h:
--- LowLevel/obj/STM32F405/startup_stm32f40xx.d
+++
@@ -1,1 +0,0 @@
-obj/STM32F405/startup_stm32f40xx.o: stm/src/startup/startup_stm32f40xx.s
