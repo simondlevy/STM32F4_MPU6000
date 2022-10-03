8d6b5e1f990a788c9ee5184bdbc505ad9ab2d3b2
simondlevy <simon.d.levy@gmail.com>
2022-10-03 19:55:18 -0400

    =Cleanup

--- STM32F405_MPU6000.ino
+++ STM32F405_MPU6000.ino
@@ -5,13 +5,13 @@
 
 #ifdef FEATHER
 static const uint8_t CS_PIN  = 10;
-#define spi1 SPI
+#define spi SPI
 #else
 static const uint8_t SCLK_PIN = PA_5;   // ???
 static const uint8_t MISO_PIN = PA_6;   // ??? 
 static const uint8_t MOSI_PIN = PA_7;   // ??? 
 static const uint8_t CS_PIN   = PA_4;   // ???
-SPIClass spi1(MOSI_PIN, MISO_PIN, SCLK_PIN);
+SPIClass spi(MOSI_PIN, MISO_PIN, SCLK_PIN);
 #endif
 
 static const uint8_t REG_PWR_MGMT_1 = 0x6B;
@@ -20,8 +20,8 @@ static const uint8_t REG_WHO_AM_I   = 0x75;
 static uint8_t readRegister(const uint8_t addr)
 {
     digitalWrite(CS_PIN, LOW);
-    spi1.transfer(addr | 0x80);
-    uint8_t result = spi1.transfer(0);
+    spi.transfer(addr | 0x80);
+    uint8_t result = spi.transfer(0);
     digitalWrite(CS_PIN, HIGH);
     return result;
 }
@@ -33,14 +33,14 @@ void setup(void)
     Serial.begin(115200);
 
 #ifdef FEATHER
-    spi1.begin();
+    spi.begin();
 #else
-    spi1.begin(1);
+    spi.begin(1);
 #endif
 
-    spi1.setBitOrder(MSBFIRST);
-    spi1.setClockDivider(SPI_CLOCK_DIV16);
-    spi1.setDataMode(SPI_MODE3);
+    spi.setBitOrder(MSBFIRST);
+    spi.setClockDivider(SPI_CLOCK_DIV16);
+    spi.setDataMode(SPI_MODE3);
 
     pinMode(CS_PIN, OUTPUT);
 
